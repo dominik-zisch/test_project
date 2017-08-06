963a9e26917ebd94e051ba02d3af3510896094ca
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-06 21:25:17 +0100

    .

--- update_native.sh
+++ update_native.sh
@@ -9,14 +9,16 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 else
     echo "Need to pull"
+
+    # kill autostart
+    ps axf | grep autostart_native | grep -v grep | awk '{print "kill -9 " $1}' | sh
+
+    # kill nativeapp
+    ps axf | grep python_test | grep -v grep | awk '{print "kill -9 " $1}' | sh
+
     git pull origin master
 fi
 
 echo "done"
 
 
-# kill nativeapp
-#ps axf | grep python_test | grep -v grep | awk '{print "kill -9 " $1}' | sh
-
-# kill autostart
-#
