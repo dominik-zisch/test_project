4d1d46e5189039fcc08c687928c11b0b3acb9cd2
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-06 23:36:32 +0100

    .

--- scripts/autostart_native.sh
+++ scripts/autostart_native.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ##### Main
 
-sh $SCRIPT_DIR/start_native.sh
+sh $SCRIPT_DIR/start_native.sh &
 sleep 10
-sh $SCRIPT_DIR/check_for_updates.sh
+sh $SCRIPT_DIR/check_for_updates.sh &
