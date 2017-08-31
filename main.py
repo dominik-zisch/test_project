7a4678b87baa197cb129f59e1ad9b5ed786a0979
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-31 16:45:13 +0100

    .

--- main.py
+++ main.py
@@ -78,7 +78,6 @@ if __name__ == '__main__':
     # main program loop
     running = True
     while ( running ):
-        print( "Second test..." )
         if ( not q.empty() ):
             topic, msg = q.get()
             print( topic + " " + str(msg) )
--- scripts/unused/test_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-NATIVE_APP='python_test'
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-##### Main
-
-sh $SCRIPT_DIR/test_2.sh $NATIVE_APP
