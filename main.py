e72042b08eade2f9363785b68339ad65cef2443d
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-31 16:42:11 +0100

    .

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     # main program loop
     running = True
     while ( running ):
-        print( "First test..." )
+        print( "Second test..." )
         if ( not q.empty() ):
             topic, msg = q.get()
             print( topic + " " + str(msg) )
